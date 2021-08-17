fa854eeccf61dba735d77b5c52c5ba4e937a63e3
Nguyen Quang Hieu <nguyenquanghieu2000d@gmail.com>
2021-08-18 00:09:48 +0700

    18082021

--- exercise/bai2.py
+++
@@ -1,9 +0,0 @@
-# Crawling dữ liệu từ các trang HTML trên báo điện tử và Phân tích nội dung text  sử dụng scrapy
-# Tham khảo các hướng dẫn:
-# http://crawler.pro.vn/view_Crawler-lay-bai-viet-tu-website-VnExpress-viet-bang-Python-va-Scrapy-that-don-gian.htm
-# http://crawler.pro.vn/view_Crawler-lay-bai-viet-tu-website-VnEconomy-viet-bang-Python-va-Scrapy-that-don-gian.htm
-#
-# Bài tập 2. Tải về 100 file HTML từ báo điện tử VnEconomy.
-# Các em nộp để tính điểm.
-
-
